030fde44c6777f71b7262191e5d3e09f5a213868
Moury <mmuya914@gmail.com>
2023-02-27 03:26:39 +0300

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0x1F2126DFb61cf1975Dc3f72bEc68137892018cf4"; // Replace with your own contract address
 const chain = 'goerli'; // goerli, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
